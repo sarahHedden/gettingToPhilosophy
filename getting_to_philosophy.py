dc6c1d350bde1bcf330214575867e8502972b2b6
sarahHedden <117850086+sarahHedden@users.noreply.github.com>
2022-11-10 05:03:04 -0600

    Ignore links to non-existent pages

--- getting_to_philosophy.py
+++ getting_to_philosophy.py
@@ -1,80 +1,84 @@
-from bs4 import BeautifulSoup
 import argparse
-import requests
-import sys
 import urllib
+import requests
+from bs4 import BeautifulSoup
 
-"""
-Finds the first link on a Wikipedia page in the main body excluding
-italicized and parenthesized links.
-params:
-    url(str): link to a wikipedia page.
-    ignore_loops(bool): skip loops instead of stopping when a loop is found.
-    visited(dict): list of already visited links.  Only used if
-        ignore_loops is set to True.
-returns: url of the first link, or None if a valid link is not found.
-"""
-def find_first_link(url: str, ignore_loops: bool, visited: dict) -> str:
-    response = requests.get(url)
-    if not response.ok:
-        print(url, "could not be reached")
-        return None
-    
-    soup = BeautifulSoup(response.content, "html.parser")
-    
-    # get main body
-    content = soup.find(id="mw-content-text").find(class_="mw-parser-output")
+def find_first_link(url: str, content: bytes, ignore_loops: bool, visited: dict) -> str:
+    """
+    Finds the first link on a Wikipedia page in the main body excluding
+    italicized and parenthesized links.
+    params:
+        url(str): link to a wikipedia page.
+        content(bytes): content on the page for the given url.
+        ignore_loops(bool): skip loops instead of stopping when a loop is found.
+        visited(dict): list of already visited links.  Only used if
+            ignore_loops is set to True.
+    returns:
+        next_link(str): url of the first link, or None if a valid link is not found.
+        content(bytes): content of the next url webpage.
+    """
+    soup = BeautifulSoup(content, "html.parser")
+    body = soup.find(id="mw-content-text").find(class_="mw-parser-output")
 
     # remove tables and italicized elements
-    for item in content.find_all("table") + content.find_all("i"):
+    for item in body.find_all("table") + body.find_all("i"):
         item.decompose()
-        
-    paragraphs = content.find_all("p", recursive=False)
+
+    paragraphs = body.find_all("p", recursive=False)
     for paragraph in paragraphs:
         links = paragraph.find_all("a")
-        text = paragraph.text
         for link in links:
-            if 'href' in link.attrs and "/wiki" in link['href'] \
-                and ":" not in link['href']:
-                index = text.find(link.text)
-                text_before = text[:index]
+            if ('href' in link.attrs and "/wiki" in link['href']
+                and ":" not in link['href']):
+                link_index = paragraph.text.find(link.text)
+                text_before = paragraph.text[:link_index]
 
                 if text_before.count("(") > text_before.count(")"):
                     # do not include parenthesized links
                     continue
-                
+
                 next_link = urllib.parse.urljoin(
                     'https://en.wikipedia.org/', link['href'])
                 if ignore_loops and next_link in visited:
                     continue
                 if next_link == url:
                     continue
-                return next_link
-    return None
 
-"""
-Follow the first links on a wikipedia page until the goal url, a loop,
-or a dead end is found.
-params:
-    url(str): the url to start at.
-    goal(str): the url to stop at.
-    max_hops(int): the number of links to follow before abandoning the search.
-    ignore_loops(bool): skip loops instead of stopping when a loop is found.
-returns: list of urls visited to reach the goal in order.  An empty list will
-    be returned if there is no valid path.
-"""
+                response = requests.get(next_link)
+                if response.ok:
+                    return next_link, response.content
+    return None, None
+
 def search_first_links(url: str = "https://en.wikipedia.org/wiki/Special:Random",
                        goal: str = "https://en.wikipedia.org/wiki/Philosophy",
                        max_hops: int = 100,
                        ignore_loops: bool = False) -> list:
+    """
+    Follow the first links on a wikipedia page until the goal url, a loop,
+    or a dead end is found.
+    params:
+        url(str): the url to start at.
+        goal(str): the url to stop at.
+        max_hops(int): the number of links to follow before abandoning the search.
+        ignore_loops(bool): skip loops instead of stopping when a loop is found.
+    returns: list of urls visited to reach the goal in order.  An empty list will
+        be returned if there is no valid path.
+    """
     visited = {url: True}
     path = [url]
     current_url = url
     hops = 0
+
+    response = requests.get(url)
+    if not response.ok:
+        print("Could not access", url)
+        return []
+    page_content = response.content
     print(url)
 
     while hops < max_hops and current_url != goal:
-        next_url = find_first_link(current_url, ignore_loops, visited)
+        next_url, page_content = find_first_link(current_url, page_content,
+                                                 ignore_loops, visited)
         if next_url is None:
             print("NO VALID LINKS!")
             return []
@@ -82,7 +86,7 @@ def search_first_links(url: str = "https://en.wikipedia.org/wiki/Special:Random"
         if next_url in visited:
             print("LOOP FROM", next_url)
             return []
-                  
+
         print(next_url)
         path.append(next_url)
         visited[next_url] = True
@@ -93,7 +97,7 @@ def search_first_links(url: str = "https://en.wikipedia.org/wiki/Special:Random"
         print("MAX HOPS EXCEEDED")
         return []
     return path
-    
+
 def main():
     parser = argparse.ArgumentParser(
         description="Follow wikipedia links to find the Philosophy page.")
@@ -113,6 +117,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-    
-    
